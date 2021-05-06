db638f82ceedb4126a928c54dcbeab10051e419b
pekellogg <pekellogg@hotmail.com>
2021-05-06 15:17:44 -0700

    commit tree 59b1ee7f7b5915328a2c26c05264e1160e8167c9

--- lib/post.rb
+++ lib/post.rb
@@ -14,7 +14,7 @@ class Post
     end
 
     def author_name
-        self.author.name if author 
+        self.author.name if self.author 
     end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -18,6 +18,6 @@ class Song
     end
 
     def artist_name
-        self.artist.name if artist
+        self.artist.name if self.artist
     end
 end
